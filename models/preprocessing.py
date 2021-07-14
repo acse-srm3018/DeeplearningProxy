b946205e23f5836d61de8732cdaf226abbc50156
Raha Moosavi <72866178+acse-srm3018@users.noreply.github.com>
2021-07-14 18:51:20 +0100

    Update preprocessing.py

--- models/preprocessing.py
+++ models/preprocessing.py
@@ -3,9 +3,51 @@ Preproceesing Data including Loading data.
 
 normalized and standardized or both of them.
 """
+
 import numpy as np
 
+def download_file_from_google_drive(id, destination):
+    URL = "https://drive.google.com/drive/folders/1A_mWXlDPWwfkjpD6I3faBn3yz_z2KHOh?usp=sharing"
 
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination) 
+    
+    
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+                
+def check_existence_and_download(id, destination):
+    file_path = Path(destination)
+    if file_path.exists():
+        print(F"{destination} is already downloaded")
+    else:
+        print(F"Downloading {destination}")
+        download_file_from_google_drive(id, destination)
+        print(F"{destination} downloaded")   
+        
+        
 def load_data(path):
     """
     Load datasets in format .NPY.
